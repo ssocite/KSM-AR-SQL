dbf40ff149901cb6d4df1692f859d4dd544ec94b
Sergio Socite <sergio.socite@kellogg.northwestern.edu>
2020-05-13 16:39:08 -0500

    Update vt_alumni_market_sheet.sql

--- vt_alumni_market_sheet.sql
+++ vt_alumni_market_sheet.sql
@@ -1,14 +1,6 @@
-create or replace view vt_ksm_phs as
-
-With KSM_PHS AS (Select comm.id_number,
-       comm.committee_code,
-       comm.committee_status_code,
-       comm.start_dt
-From committee comm
-Where comm.committee_code IN ('KPH')
-And comm.committee_status_code = 'C'),
-
-
+create or replace view vt_alumni_market_sheet as
+With
+-- Employment table subquery
 employ As (
   Select id_number
   , job_title
@@ -27,77 +19,92 @@ employ As (
   From employment
   Left Join tms_fld_of_work fow
        On fow.fld_of_work_code = employment.fld_of_work_code
-  Where employment.primary_emp_ind = 'Y'),
+  Where employment.primary_emp_ind = 'Y'
+)
 
-KSM_Assignment AS (
-Select summary.prospect_id,
-       summary.id_number,
-       summary.prospect_manager_id,
-       summary.prospect_manager,
-       summary.lgo_ids,
-       summary.lgos,
-       summary.manager_ids,
-       summary.managers,
-       summary.curr_ksm_manager
-From rpt_pbh634.v_assignment_summary summary),
+--- Degree Fields
 
-KSM_Model As (
-select prospect.ID_NUMBER,
-prospect.mgo_pr_model,
-prospect.mgo_pr_score
-from rpt_pbh634.v_ksm_prospect_pool prospect)
+Select Distinct
+rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER,
 
-Select house.ID_NUMBER,
-       house.REPORT_NAME,       
-       house.RECORD_STATUS_CODE,
-       entity.gender_code,
-       KSM_PHS.committee_code,
-       KSM_PHS.committee_status_code,
-       KSM_PHS.start_dt,
-       house.HOUSEHOLD_KSM_YEAR,
-       house.HOUSEHOLD_MASTERS_YEAR,
-       house.HOUSEHOLD_PROGRAM,
-       house.HOUSEHOLD_PROGRAM_GROUP,
-       employ.employer_name,
-       employ.job_title,
-       employ.fld_of_work,
-       house.HOUSEHOLD_CITY,
-       house.HOUSEHOLD_STATE,
-       house.HOUSEHOLD_ZIP,
-       house.HOUSEHOLD_GEO_CODES,
-       house.HOUSEHOLD_GEO_PRIMARY,
-       house.HOUSEHOLD_COUNTRY,
-       house.HOUSEHOLD_CONTINENT,
-       KSM_Assignment.prospect_manager,
-       KSM_Assignment.lgos,
-       KSM_Assignment.managers,
-       KSM_Assignment.curr_ksm_manager,
-       give.NGC_LIFETIME,
-       give.NGC_CFY,
-       give.NGC_PFY1,
-       give.NGC_PFY2,
-       give.NGC_PFY3,
-       give.NGC_PFY4,
-       give.NGC_PFY5,
-       give.AF_CFY,
-       give.AF_PFY1,
-       give.AF_PFY2,
-       give.AF_PFY3,
-       give.AF_PFY4,
-       give.AF_PFY5,
-       give.LAST_GIFT_DATE,
-       give.LAST_GIFT_TYPE,
-       give.LAST_GIFT_ALLOC_CODE,
-       KSM_Model.mgo_pr_model,
-       KSM_Model.mgo_pr_score
-From rpt_pbh634.v_entity_ksm_households house
-Inner Join KSM_PHS ON KSM_PHS.id_number = house.id_number
-Left Join employ ON employ.id_number = house.ID_NUMBER
-Left Join entity on entity.id_number = house.ID_NUMBER
-Left Join KSM_Assignment ON KSM_Assignment.id_number = house.ID_NUMBER
-Left Join  rpt_pbh634.v_ksm_giving_summary give on give.ID_NUMBER = house.id_number
-Left Join  KSM_Model ON KSM_Model.id_number = house.ID_NUMBER
-Order by house.REPORT_NAME ASC;
+rpt_pbh634.v_entity_ksm_degrees.REPORT_NAME,
+
+rpt_pbh634.v_entity_ksm_degrees.RECORD_STATUS_CODE,
+
+rpt_pbh634.v_entity_ksm_degrees.FIRST_KSM_YEAR,
+
+rpt_pbh634.v_entity_ksm_degrees.PROGRAM,
+
+rpt_pbh634.v_entity_ksm_degrees.PROGRAM_GROUP,
+
+--- Gender Code
+
+Entity.Gender_Code,
+
+--- Employment
+
+Employ.fld_of_work_code,
+
+Employ.fld_of_work,
+
+Employ.employer_name,
+
+Employ.job_title,
+
+--- City, State, Zip, Geo Codes By Household
+
+rpt_pbh634.v_entity_ksm_households.HOUSEHOLD_CITY,
+
+rpt_pbh634.v_entity_ksm_households.HOUSEHOLD_STATE,
+
+rpt_pbh634.v_entity_ksm_households.HOUSEHOLD_ZIP,
+
+rpt_pbh634.v_entity_ksm_households.HOUSEHOLD_GEO_CODES,
+
+rpt_pbh634.v_entity_ksm_households.HOUSEHOLD_COUNTRY,
+
+rpt_pbh634.v_entity_ksm_households.HOUSEHOLD_CONTINENT,
+
+rpt_pbh634.v_ksm_prospect_pool.prospect_manager_id,
+
+rpt_pbh634.v_ksm_prospect_pool.prospect_manager,
+
+rpt_pbh634.v_ksm_prospect_pool.mgo_pr_score,
+
+rpt_pbh634.v_ksm_prospect_pool.mgo_pr_model,
+
+vt_leadership_giving_officer.assignment_id_number,
+
+vt_leadership_giving_officer.Leadership_Giving_Officer
 
 
+From rpt_pbh634.v_entity_ksm_degrees
 
+---- Join Entity
+
+Inner Join Entity on rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER = Entity.Id_Number
+
+---- Join Households
+
+Left Join rpt_pbh634.v_entity_ksm_households On rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER = rpt_pbh634.v_entity_ksm_households.ID_NUMBER
+
+---- Join Employment
+
+Left Join Employ On rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER = Employ.Id_Number
+
+---- Join Prospect
+
+Left Join rpt_pbh634.v_ksm_prospect_pool on rpt_pbh634.v_ksm_prospect_pool.ID_NUMBER = rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER
+
+---- Join Assignment
+
+Left Join Assignment on assignment.id_number = rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER
+
+--- Join Leadership Giving Officer
+
+Full Outer Join vt_leadership_giving_officer ON vt_leadership_giving_officer.id_number = rpt_pbh634.v_entity_ksm_degrees.ID_NUMBER
+
+---- Active Alumni
+
+Where rpt_pbh634.v_entity_ksm_degrees.Record_Status_Code = 'A'
+;
